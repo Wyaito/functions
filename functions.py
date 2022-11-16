7e0339c4f47f6856af981efd7e9bfae9326080bf
Wyatt Lynch <wyguy1121@gmail.com>
2022-11-15 21:00:47 -0500

    ask user for unit

---
+++ functions.py
@@ -0,0 +1,18 @@
+# conversion calulator 
+#By: Wyatt Lynch
+
+# user input regarding the length to convert
+
+# get the unit from the user
+
+#convert the length to the correct unit
+
+# output the answer to the user
+
+user_number = input("What number to convert? ")
+userUnit = input("What unit is your number? ")
+
+
+
+print(user_number)
+print(userUnit)
