8ecb0b35dcffb4f364b7f308a95e9453a74c1d66
Wyatt Lynch <wyguy1121@gmail.com>
2022-11-15 21:17:10 -0500

    incorect unit else

--- functions.py
+++ functions.py
@@ -2,22 +2,24 @@
 #By: Wyatt Lynch
 
 # user input regarding the length to convert
-
-# get the unit from the user
-
-#convert the length to the correct unit
-
-# output the answer to the user
-
 user_number = float(input("What number to convert? "))
+# get the unit from the user
 userUnit = input("What unit is your number? ")
-
+#convert the length to the correct unit
 if(userUnit == "in"):
     #perform in to mm
     convNumber = user_number * 25.4
+    convUnit = 'mm'
 elif(userUnit == 'mm'):
     #perform mm to in
     convNumber = user_number / 25.4
+    convUnit = 'in'
+else: 
+    print('that is not a valid unit')
+# output the answer to the user
+print(convNumber, convUnit)
 
-print(convNumber)
-print(userUnit)
+
+
+
+
